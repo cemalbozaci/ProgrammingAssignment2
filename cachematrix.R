38f571a84ec8148eaa75eb8df65ef87a1d32c701
Cemal Bozaci <cemalbozaci@yahoo.com>
2018-01-23 13:15:29 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(!is.matrix(x)){
+    message("please make sure your input in the form of matrix(c(2,3,1,4),nrow=2,ncol=2)")
+  }
+  i = NULL
+  assign_catched = function(catched) i <<- catched
+  get_catched = function() i 
+  get_data = function() x
+  list(assign_catched = assign_catched, get_catched = get_catched,
+       get_data = get_data)
 }
 
 
@@ -12,4 +20,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+i = x$get_catched()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get_data()
+  i <- solve(data, ...)
+  x$assign_catched(i)
+  i
 }
